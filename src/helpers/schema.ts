1f3cda88e1d0c7c7d86df287326caedcb9541291
Jovi De Croock <jovi.decroock@formidable.com>
2019-08-06 14:41:41 +0100

    (refactor) - generate more minimalistic result from the introspectionQuery

--- src/helpers/schema.ts
+++ src/helpers/schema.ts
@@ -1,43 +1,111 @@
-import {
-  getIntrospectionQuery,
-  buildClientSchema,
-  printSchema,
-  parse,
-  DocumentNode,
-} from 'graphql';
+import { buildClientSchema, printSchema, parse, DocumentNode } from 'graphql';
 
-export const getSchema = async (schemaUrl: string): Promise<DocumentNode> => {
-  const result = await fetch(schemaUrl, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      query: getIntrospectionQuery({ descriptions: false }),
-    }),
-  });
-
-  if (result.status < 200 || result.status >= 300) {
-    throw new Error(result.statusText);
-  }
-
-  const { data: schema, errors } = await result.json();
-
-  if (errors) {
-    console.error(errors);
-    // TODO: we need to check what errors can be present here.
-    throw new Error(`Something went wrong while fetching your schema.`);
-  }
-
-  return schema;
-};
-
-// TODO: kitten said we are pulling in too much like this,
-// this has to be stripped down.
-// Based on what "schema" in the main method is we might now
-// want to parse here.
-// this effectively means pulling in the graphql-introspection
-// query and making one ourselves.
 export const parseSchema = schema =>
   parse(printSchema(buildClientSchema(schema)));
+
+export const getSchema = (schemaUrl: string): Promise<DocumentNode> => {
+  return new Promise((resolve, reject) => {
+    fetch(schemaUrl, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: introspectionQuery,
+      }),
+    })
+      .then(result => {
+        if (result.status < 200 || result.status >= 300) {
+          reject(result.statusText);
+        }
+        return result.json();
+      })
+      .then(({ data: schema, errors }) => {
+        if (errors) {
+          console.error(errors);
+          // TODO: we need to check what errors can be present here.
+          reject(`Something went wrong while fetching your schema.`);
+        }
+        resolve(schema);
+      });
+  });
+};
+
+const introspectionQuery = `
+  query IntrospectionQuery {
+    __schema {
+      queryType { name }
+      mutationType { name }
+      subscriptionType { name }
+      types {
+        ...FullType
+      }
+    }
+  }
+
+  fragment FullType on __Type {
+    kind
+    name
+    fields(includeDeprecated: true) {
+      name
+      args {
+        ...InputValue
+      }
+      type {
+        ...TypeRef
+      }
+    }
+    inputFields {
+      ...InputValue
+    }
+    interfaces {
+      ...TypeRef
+    }
+    enumValues(includeDeprecated: true) {
+      name
+    }
+    possibleTypes {
+      ...TypeRef
+    }
+  }
+
+  fragment InputValue on __InputValue {
+    name
+    type { ...TypeRef }
+    defaultValue
+  }
+
+  fragment TypeRef on __Type {
+    kind
+    name
+    ofType {
+      kind
+      name
+      ofType {
+        kind
+        name
+        ofType {
+          kind
+          name
+          ofType {
+            kind
+            name
+            ofType {
+              kind
+              name
+              ofType {
+                kind
+                name
+                ofType {
+                  kind
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
