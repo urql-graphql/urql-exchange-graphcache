1078a39fc1e76090bc9f98fcc3b1fb8bbc7819c5
Jovi De Croock <jovi.decroock@formidable.com>
2019-08-09 10:15:25 +0100

    (tests) - add initial test for queryResolvers

--- src/test-utils/examples-1.test.ts
+++ src/test-utils/examples-1.test.ts
@@ -75,3 +75,64 @@ it('passes the "getting-started" example', () => {
     todos: [...todosData.todos.slice(0, 2), mutatedTodo],
   });
 });
+
+it('Respects resolvers when given', () => {
+  const store = new Store(undefined, { Todo: { text: () => 'hi' } });
+  const todosData = {
+    __typename: 'Query',
+    todos: [
+      { id: '0', text: 'Go to the shops', complete: false, __typename: 'Todo' },
+      { id: '1', text: 'Pick up the kids', complete: true, __typename: 'Todo' },
+      { id: '2', text: 'Install urql', complete: false, __typename: 'Todo' },
+    ],
+  };
+
+  const writeRes = write(store, { query: Todos }, todosData);
+
+  const expectedSet = new Set(['Query.todos', 'Todo:0', 'Todo:1', 'Todo:2']);
+  expect(writeRes.dependencies).toEqual(expectedSet);
+
+  expect(store.serialize()).toMatchSnapshot();
+
+  let queryRes = query(store, { query: Todos });
+
+  expect(queryRes.data).toEqual({
+    __typename: 'Query',
+    todos: [
+      { id: '0', text: 'hi', complete: false, __typename: 'Todo' },
+      { id: '1', text: 'hi', complete: true, __typename: 'Todo' },
+      { id: '2', text: 'hi', complete: false, __typename: 'Todo' },
+    ],
+  });
+  expect(queryRes.dependencies).toEqual(writeRes.dependencies);
+  expect(queryRes.completeness).toBe('FULL');
+
+  const mutatedTodo = {
+    ...todosData.todos[2],
+    complete: true,
+  };
+
+  const mutationRes = write(
+    store,
+    { query: ToggleTodo, variables: { id: '2' } },
+    {
+      __typename: 'Mutation',
+      toggleTodo: mutatedTodo,
+    }
+  );
+
+  expect(mutationRes.dependencies).toEqual(new Set(['Todo:2']));
+  expect(store.serialize()).toMatchSnapshot();
+
+  queryRes = query(store, { query: Todos });
+
+  expect(queryRes.completeness).toBe('FULL');
+  expect(queryRes.data).toEqual({
+    ...todosData,
+    todos: [
+      { id: '0', text: 'hi', complete: false, __typename: 'Todo' },
+      { id: '1', text: 'hi', complete: true, __typename: 'Todo' },
+      { id: '2', text: 'hi', complete: true, __typename: 'Todo' },
+    ],
+  });
+});
