6865f412a446a1057c8c814f050c5337e8082173
Jovi De Croock <jovi.decroock@formidable.com>
2019-08-19 16:43:03 +0200

    (tests) - basic matching without schema to be aware of

---
+++ src/fragments/matcher.test.ts
@@ -0,0 +1,80 @@
+import gql from 'graphql-tag';
+import { matchFragment } from './matcher';
+import { Store } from '../store';
+import { write } from '../operations';
+
+const todosData = {
+  __typename: 'Query',
+  todos: [
+    {
+      id: '0',
+      text: 'Go to the shops',
+      complete: false,
+      __typename: 'Todo',
+      author: { id: '0', name: 'Jovi', __typename: 'Author' },
+    },
+    {
+      id: '1',
+      text: 'Pick up the kids',
+      complete: true,
+      __typename: 'Todo',
+      author: { id: '1', name: 'Phil', __typename: 'Author' },
+    },
+    {
+      id: '2',
+      text: 'Install urql',
+      complete: false,
+      __typename: 'Todo',
+      author: { id: '0', name: 'Jovi', __typename: 'Author' },
+    },
+  ],
+};
+
+describe('fragmentMatcher', () => {
+  let store, query;
+  beforeEach(() => {
+    store = new Store();
+    query = gql`
+      query {
+        todos {
+          id
+          __typename
+          ...TodoData
+        }
+      }
+      fragment TodoData on Todo {
+        text
+        complete
+      }
+    `;
+    write(store, { query }, todosData);
+  });
+
+  it('can match a fragment', () => {
+    const fragmentQuery = gql`
+      fragment TodoData on Todo {
+        text
+        complete
+      }
+    `;
+    expect(matchFragment(store, 'Todo:1', fragmentQuery.definitions[0])).toBe(
+      true
+    );
+  });
+
+  it('returns true for Query', () => {
+    const fragmentQuery = gql`
+      fragment Todos on Query {
+        todos {
+          id
+          text
+          complete
+          __typename
+        }
+      }
+    `;
+    expect(matchFragment(store, 'Query', fragmentQuery.definitions[0])).toBe(
+      true
+    );
+  });
+});
