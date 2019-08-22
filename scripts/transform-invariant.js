ec4cd0be41d048ddbf12cf6f357d9cb8cf04b34e
Jovi De Croock <jovi.decroock@formidable.com>
2019-08-22 11:53:37 +0200

    (feat) - make invariant babel plugin

---
+++ scripts/transform-invariant.js
@@ -0,0 +1,29 @@
+const visited = 'visitedByInvariantTransformer';
+const pragma = 'invariant';
+
+export default function Plugin({ template }) {
+  const wrapWithDevCheck = template(
+      `
+    if (process.env.NODE_ENV !== "production") {
+      NODE;
+    }
+  `,
+    { placeholderPattern: /^NODE$/ }
+  );
+
+  return {
+    visitor: {
+      Program(p) {
+        p.traverse({
+          CallExpression(path) {
+            if (path.node[visited]) return
+            path.node[visited] = true;
+            if (path.node.callee.name === pragma) {
+              path.replaceWith(wrapWithDevCheck({ NODE: path.node }));
+            }
+          }
+        })
+      },
+    },
+  };
+}
