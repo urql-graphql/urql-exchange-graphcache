0fc92037451327c8f14a76ce2aa32fbfb08a1fe4
Max Stoiber <mxstbr@github.com>
2019-09-26 09:26:30 +0200

    Add failing test for fetching duplicate edges

--- src/extras/relayPagination.test.ts
+++ src/extras/relayPagination.test.ts
@@ -170,3 +170,105 @@ it('works with backwards pagination', () => {
     },
   });
 });
+
+it('handles duplicate edges', () => {
+  const Pagination = gql`
+    query($cursor: String) {
+      items(first: 2, after: $cursor) {
+        __typename
+        edges {
+          __typename
+          node {
+            __typename
+            id
+          }
+        }
+        pageInfo {
+          __typename
+          hasNextPage
+          endCursor
+        }
+      }
+    }
+  `;
+
+  const store = new Store(undefined, {
+    Query: {
+      items: relayPagination(),
+    },
+  });
+
+  const pageOne = {
+    __typename: 'Query',
+    items: {
+      __typename: 'ItemsConnection',
+      edges: [
+        {
+          __typename: 'ItemEdge',
+          node: {
+            __typename: 'Item',
+            id: '1',
+          },
+        },
+        {
+          __typename: 'ItemEdge',
+          node: {
+            __typename: 'Item',
+            id: '2',
+          },
+        },
+      ],
+      pageInfo: {
+        __typename: 'PageInfo',
+        hasNextPage: true,
+        endCursor: '2',
+      },
+    },
+  };
+
+  const pageTwo = {
+    __typename: 'Query',
+    items: {
+      __typename: 'ItemsConnection',
+      edges: [
+        {
+          __typename: 'ItemEdge',
+          node: {
+            __typename: 'Item',
+            id: '2',
+          },
+        },
+        {
+          __typename: 'ItemEdge',
+          node: {
+            __typename: 'Item',
+            id: '3',
+          },
+        },
+      ],
+      pageInfo: {
+        __typename: 'PageInfo',
+        hasNextPage: false,
+        endCursor: null,
+      },
+    },
+  };
+
+  write(store, { query: Pagination, variables: { cursor: null } }, pageOne);
+  write(store, { query: Pagination, variables: { cursor: '1' } }, pageTwo);
+
+  const res = query(store, { query: Pagination });
+
+  expect(res.partial).toBe(false);
+  expect(res.data).toEqual({
+    ...pageTwo,
+    items: {
+      ...pageTwo.items,
+      edges: [
+        pageOne.items.edges[0],
+        pageTwo.items.edges[0],
+        pageTwo.items.edges[1],
+      ],
+    },
+  });
+});
