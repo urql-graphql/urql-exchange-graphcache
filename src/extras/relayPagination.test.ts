eb98b00076cf423993b9da902cd1326fea720059
Max Stoiber <mxstbr@github.com>
2019-09-26 09:21:00 +0200

    Add failing test for simultaneous forwards- and backwards pagination

--- src/extras/relayPagination.test.ts
+++ src/extras/relayPagination.test.ts
@@ -242,3 +242,131 @@ it('falls back to cursor field instead of PageInfo cursors', () => {
     },
   });
 });
+
+it('works with simultaneous forward and backward pagination', () => {
+  const Pagination = gql`
+    query($first: Int, $last: Int, $before: String, $after: String) {
+      items(first: $first, last: $last, before: $before, after: $after) {
+        __typename
+        edges {
+          __typename
+          node {
+            __typename
+            id
+          }
+        }
+        pageInfo {
+          __typename
+          hasNextPage
+          endCursor
+        }
+      }
+    }
+  `;
+
+  const store = new Store(undefined, {
+    Query: {
+      items: relayPagination(),
+    },
+  });
+
+  const pageOne = {
+    __typename: 'Query',
+    items: {
+      __typename: 'ItemsConnection',
+      edges: [
+        {
+          __typename: 'ItemEdge',
+          node: {
+            __typename: 'Item',
+            id: '1',
+          },
+        },
+      ],
+      pageInfo: {
+        __typename: 'PageInfo',
+        hasNextPage: true,
+        hasPreviousPage: false,
+        startCursor: null,
+        endCursor: '1',
+      },
+    },
+  };
+
+  const pageTwo = {
+    __typename: 'Query',
+    items: {
+      __typename: 'ItemsConnection',
+      edges: [
+        {
+          __typename: 'ItemEdge',
+          node: {
+            __typename: 'Item',
+            id: '2',
+          },
+        },
+      ],
+      pageInfo: {
+        __typename: 'PageInfo',
+        hasNextPage: true,
+        hasPreviousPage: true,
+        startCursor: '2',
+        endCursor: '2',
+      },
+    },
+  };
+
+  const pageThree = {
+    __typename: 'Query',
+    items: {
+      __typename: 'ItemsConnection',
+      edges: [
+        {
+          __typename: 'ItemEdge',
+          node: {
+            __typename: 'Item',
+            id: '3',
+          },
+        },
+      ],
+      pageInfo: {
+        __typename: 'PageInfo',
+        hasNextPage: false,
+        hasPreviousPage: true,
+        endCursor: null,
+        startCursor: '3',
+      },
+    },
+  };
+
+  write(
+    store,
+    { query: Pagination, variables: { after: '1', first: 1 } },
+    pageTwo
+  );
+  write(
+    store,
+    { query: Pagination, variables: { after: '2', first: 1 } },
+    pageThree
+  );
+  write(
+    store,
+    { query: Pagination, variables: { before: '1', last: 1 } },
+    pageOne
+  );
+
+  const res = query(store, { query: Pagination });
+
+  expect(res.partial).toBe(false);
+  expect(res.data).toEqual({
+    ...pageOne,
+    items: {
+      ...pageOne.items,
+      edges: [
+        pageOne.items.edges[0],
+        pageTwo.items.edges[0],
+        pageThree.items.edges[0],
+      ],
+    },
+  });
+});
