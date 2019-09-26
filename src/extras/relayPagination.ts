679e459adab41d978473bba413543f9f0316136a
Phil Pluckthun <phil@kitten.sh>
2019-09-26 16:30:51 +0100

    Implement Relay algorithm for concatenating pages
    See Relay source for original algorithm:
    https://github.com/facebook/relay/blob/7153ce1/packages/relay-runtime/store/RelayConnectionResolver.js#L66-L102

--- src/extras/relayPagination.ts
+++ src/extras/relayPagination.ts
@@ -5,55 +5,67 @@ import { joinKeys, keyOfField } from '../helpers';
 interface PageInfo {
   endCursor: null | string;
   startCursor: null | string;
-  hasNextPage: null | boolean;
-  hasPreviousPage: null | boolean;
+  hasNextPage: boolean;
+  hasPreviousPage: boolean;
 }
 
 interface Page {
-  __typename: string;
   edges: NullArray<string>;
   pageInfo: PageInfo;
 }
+
+const defaultPageInfo: PageInfo = {
+  endCursor: null,
+  startCursor: null,
+  hasNextPage: false,
+  hasPreviousPage: false,
+};
+
+const ensureKey = (x: any): string | null => (typeof x === 'string' ? x : null);
 
 const getPage = (cache: Store, linkKey: string): Page | null => {
   const link = cache.getLink(linkKey);
   if (!link || Array.isArray(link)) return null;
 
-  const typename = cache.resolve(link, '__typename');
-  const edges = cache.resolve(link, 'edges');
-
+  const edges = cache.resolve(link, 'edges') as NullArray<string>;
   if (
-    typeof typename !== 'string' ||
     !Array.isArray(edges) ||
     !edges.every(x => x === null || typeof x === 'string')
   ) {
     return null;
   }
 
-  const pageInfo = cache.resolve(link, 'pageInfo');
-  const page: Page = {
-    __typename: typename,
-    edges: edges as NullArray<string>,
-    pageInfo: {
-      endCursor: null,
-      startCursor: null,
-      hasNextPage: null,
-      hasPreviousPage: null,
-    },
-  };
+  const page: Page = { edges, pageInfo: defaultPageInfo };
+  const pageInfoKey = cache.resolve(link, 'pageInfo');
+  if (typeof pageInfoKey === 'string') {
+    const endCursor = ensureKey(cache.resolve(pageInfoKey, 'endCursor'));
+    const startCursor = ensureKey(cache.resolve(pageInfoKey, 'startCursor'));
+    const hasNextPage = cache.resolve(pageInfoKey, 'hasNextPage');
+    const hasPreviousPage = cache.resolve(pageInfoKey, 'hasPreviousPage');
 
-  if (typeof pageInfo === 'string') {
-    const endCursor = cache.resolve(pageInfo, 'endCursor');
-    const startCursor = cache.resolve(pageInfo, 'startCursor');
-    const hasNextPage = cache.resolve(pageInfo, 'hasNextPage');
-    const hasPreviousPage = cache.resolve(pageInfo, 'hasPreviousPage');
-    page.pageInfo.endCursor = typeof endCursor === 'string' ? endCursor : null;
-    page.pageInfo.startCursor =
-      typeof startCursor === 'string' ? startCursor : null;
-    page.pageInfo.hasNextPage =
-      typeof hasNextPage === 'boolean' ? hasNextPage : null;
-    page.pageInfo.hasPreviousPage =
-      typeof hasPreviousPage === 'boolean' ? hasPreviousPage : null;
+    const pageInfo: PageInfo = (page.pageInfo = {
+      hasNextPage: typeof hasNextPage === 'boolean' ? hasNextPage : !!endCursor,
+      hasPreviousPage:
+        typeof hasPreviousPage === 'boolean' ? hasPreviousPage : !!startCursor,
+      endCursor,
+      startCursor,
+    });
+
+    if (pageInfo.endCursor === null) {
+      const edge = edges[edges.length - 1];
+      if (edge) {
+        const endCursor = cache.resolve(edge, 'cursor');
+        pageInfo.endCursor = ensureKey(endCursor);
+      }
+    }
+
+    if (pageInfo.startCursor === null) {
+      const edge = edges[0];
+      if (edge) {
+        const startCursor = cache.resolve(edge, 'cursor');
+        pageInfo.startCursor = ensureKey(startCursor);
+      }
+    }
   }
 
   return page;
@@ -62,18 +74,63 @@ const getPage = (cache: Store, linkKey: string): Page | null => {
 export const relayPagination = (): Resolver => {
   return (_parent, args, cache, info) => {
     const { parentKey: key, fieldName } = info;
+    const fieldKey = joinKeys(key, keyOfField(fieldName, args));
     const connections = cache.resolveConnections(key, fieldName);
-    if (connections.length === 0) return undefined;
-    let connection;
-    const fieldKey = joinKeys(info.parentKey, keyOfField(info.fieldName, args));
+    const size = connections.length;
 
-    // Use find so we can short-circuit early if needed.
-    connections.find(con => {
-      if (fieldKey === con[1]) return (connection = con[1]);
-    });
+    let hasCachedConnection = false;
+    for (let i = 0; i < size; i++) {
+      if (connections[i][1] === fieldKey) {
+        hasCachedConnection = true;
+        break;
+      }
+    }
 
-    if (!connection) return undefined;
-    const page = getPage(cache, connection);
-    return page ? (page as any) : undefined;
+    const entityKey = cache.resolveValueOrLink(fieldKey);
+    if (!hasCachedConnection || typeof entityKey !== 'string') {
+      return undefined;
+    }
+
+    const typename = cache.resolve(entityKey, '__typename');
+
+    const pageInfoKey = ensureKey(cache.resolve(entityKey, 'pageInfo'));
+    const pageInfoTypename = cache.resolve(pageInfoKey, '__typename');
+    if (typeof typename !== 'string' || typeof pageInfoTypename !== 'string') {
+      return undefined;
+    }
+
+    let edges: NullArray<string> = [];
+    let pageInfo: PageInfo = { ...defaultPageInfo };
+
+    for (let i = 0; i < size; i++) {
+      const [args, linkKey] = connections[i];
+      const page = getPage(cache, linkKey);
+      if (page === null) {
+        continue;
+      } else if (args.after) {
+        edges = edges.concat(page.edges);
+        pageInfo.endCursor = page.pageInfo.endCursor;
+        pageInfo.hasNextPage = page.pageInfo.hasNextPage;
+      } else if (args.before) {
+        edges = page.edges.concat(edges);
+        pageInfo.startCursor = page.pageInfo.startCursor;
+        pageInfo.hasPreviousPage = page.pageInfo.hasPreviousPage;
+      } else if (!args.before && !args.after) {
+        edges = edges.concat(page.edges);
+        pageInfo = page.pageInfo;
+      }
+    }
+
+    return {
+      __typename: typename,
+      edges,
+      pageInfo: {
+        __typename: pageInfoTypename,
+        endCursor: pageInfo.endCursor,
+        startCursor: pageInfo.startCursor,
+        hasNextPage: pageInfo.hasNextPage,
+        hasPreviousPage: pageInfo.hasPreviousPage,
+      },
+    };
   };
 };
