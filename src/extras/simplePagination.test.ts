0f86bf16593a9a07c03999daab05d86364a5aefd
jovi De Croock <decroockjovi@gmail.com>
2019-11-18 11:45:50 +0100

    allow for prepending

--- src/extras/simplePagination.test.ts
+++ src/extras/simplePagination.test.ts
@@ -109,6 +109,59 @@ it('handles duplicates', () => {
   });
 });
 
+it('should preserve the correct order', () => {
+  const Pagination = gql`
+    query($from: Number, $limit: Number) {
+      persons(from: $from, limit: $limit) {
+        __typename
+        id
+        name
+      }
+    }
+  `;
+
+  const store = new Store(undefined, {
+    Query: {
+      persons: simplePagination(),
+    },
+  });
+
+  const pageOne = {
+    __typename: 'Query',
+    persons: [
+      { id: 1, name: 'Jovi', __typename: 'Person' },
+      { id: 2, name: 'Phil', __typename: 'Person' },
+      { id: 3, name: 'Andy', __typename: 'Person' },
+    ],
+  };
+
+  const pageTwo = {
+    __typename: 'Query',
+    persons: [
+      { id: 4, name: 'Kadi', __typename: 'Person' },
+      { id: 5, name: 'Dom', __typename: 'Person' },
+      { id: 6, name: 'Sofia', __typename: 'Person' },
+    ],
+  };
+
+  write(
+    store,
+    { query: Pagination, variables: { from: 3, limit: 3 } },
+    pageTwo
+  );
+  write(
+    store,
+    { query: Pagination, variables: { from: 0, limit: 3 } },
+    pageOne
+  );
+
+  const result = query(store, { query: Pagination });
+  expect(result.data).toEqual({
+    __typename: 'Query',
+    persons: [...pageOne.persons, ...pageTwo.persons],
+  });
+});
+
 it('prevents overlapping of pagination on different arguments', () => {
   const Pagination = gql`
     query($from: Number, $limit: Number, $filter: string) {
--- src/extras/simplePagination.ts
+++ src/extras/simplePagination.ts
@@ -1,21 +1,21 @@
 import { stringifyVariables } from 'urql';
-import { Resolver, Variables } from '../types';
+import { Resolver, Variables, NullArray } from '../types';
 
 export interface PaginationParams {
-  fromName?: string;
-  limitName?: string;
+  offsetArgument?: string;
+  limitArgument?: string;
 }
 
-export const simplePagination = (params?: PaginationParams): Resolver => {
-  const from = (params && params.fromName) || 'from';
-  const limit = (params && params.limitName) || 'limit';
-
+export const simplePagination = ({
+  offsetArgument = 'from',
+  limitArgument = 'limit',
+}: PaginationParams = {}): Resolver => {
   const compareArgs = (
     fieldArgs: Variables,
     connectionArgs: Variables
   ): boolean => {
     for (const key in connectionArgs) {
-      if (key === from || key === limit) {
+      if (key === offsetArgument || key === limitArgument) {
         continue;
       } else if (!(key in fieldArgs)) {
         return false;
@@ -40,22 +40,36 @@ export const simplePagination = (params?: PaginationParams): Resolver => {
     const { parentKey: key, fieldName } = info;
     const connections = cache.resolveConnections(key, fieldName);
     const size = connections.length;
-    const result: string[] = [];
+    let result: NullArray<string> = [];
     const visited = new Set();
+    let prevOffset: number | null = null;
     if (size === 0) return undefined;
 
     for (let i = 0; i < size; i++) {
       const [args, linkKey] = connections[i];
       if (!compareArgs(fieldArgs, args)) continue;
       const links = cache.resolveValueOrLink(linkKey) as string[];
-      if (links.length === 0) return undefined;
-      result.push(
-        ...links.reduce<string[]>((acc, lk) => {
-          if (visited.has(lk)) return acc;
-          visited.add(lk);
-          return [...acc, lk];
-        }, [])
-      );
+      if (links === null || links.length === 0) continue;
+
+      if (!prevOffset || (args as any)[offsetArgument] > prevOffset) {
+        for (let j = 0; j < links.length; j++) {
+          const link = links[j];
+          if (visited.has(link)) continue;
+          result.push(link);
+          visited.add(link);
+        }
+      } else {
+        const tempResult: NullArray<string> = [];
+        for (let j = 0; j < links.length; j++) {
+          const link = links[j];
+          if (visited.has(link)) continue;
+          tempResult.push(link);
+          visited.add(link);
+        }
+        result = [...tempResult, ...result];
+      }
+
+      prevOffset = (args as any)[offsetArgument];
     }
 
     return result;
